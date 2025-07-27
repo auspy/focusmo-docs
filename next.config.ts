140ef1bacaa5e647031f90ada1917286a2ca7369
auspy <vinayak.kshetez0102@gmail.com>
2025-07-27 12:38:57 +0530

    feat: setup complete

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import nextra from "nextra";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const withNextra = nextra({});
 
-export default nextConfig;
+export default withNextra({
+  // turbopack: {
+  //   resolveAlias: {
+  //     // Path to your `mdx-components` file with extension
+  //     "next-mdx-import-source-file": "./mdx-components.tsx",
+  //   },
+  // },
+});
